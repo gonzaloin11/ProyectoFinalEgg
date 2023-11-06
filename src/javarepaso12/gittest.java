098f846ac1e701a1dd9d8aae12e90dabe745d059
Gonzalo.i <gonzalo.insua11@gmail.com>
2023-11-06 15:46:54 -0300

    segundo commit

---
+++ src/javarepaso12/gittest.java
@@ -0,0 +1,20 @@
+
+package javarepaso12;
+
+
+public class gittest {
+    
+      public static void main(String[] args) {
+    
+  String git="Soy un git";
+          System.out.println(""+git);
+    
+       
+    
+    
+               
+    
+  
+    
+}
+}
